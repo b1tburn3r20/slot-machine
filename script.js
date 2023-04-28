263acc04bd6b47d7be6c2c2d84fe6b894e62c7c6
Alejandro Bermudez <118564985+b1tburn3r20@users.noreply.github.com>
2023-04-28 13:39:28 -0500

    removed console logs

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
+
 //constants
+
+
 const items = {
     '🔥':50,
     '🍋':100,
     '🎰':1000,
     '🦧':-200    
 }
+
+
 //cached items
+
+
 spinBtn = document.getElementById('spinBtn')
 slots = document.querySelectorAll('.slot')
 scoreDisplay = document.querySelector('.score')
-//event listeners
-spinBtn.addEventListener('click', luckySpin)
-//state variables
-score = 20
-function luckySpin(){
-    score -= 2;
-    scoreDisplay.innerText = `Money: $${score}`;
-    setTimeout(spin, 10);
-    console.log('luckyspin')
 
+
+//event listeners
+
+
+spinBtn.addEventListener('click', luckySpin)
+
+
+//state variables
+
+score = 20
+
+//functions
+
+
+function luckySpin(){
+    scoreDisplay.innerText = `Money: $${score}`;
+    if(score <= 0){
+        alert(`You're broke. Make this a lesson to not gamble.`)
+    return
+    }
+    score -= 2;
+    setTimeout(spin, 1);
 }
 function spin() {
     console.log('spintop')
@@ -27,15 +47,12 @@ function spin() {
         const randomIndex = Math.floor(Math.random()*emojis.length)
         const randomEmoji = emojis[randomIndex]
         slot.innerText = randomEmoji   
-        console.log('spinend')
-
     }
     checkWin()
   }
   
   
 function checkWin(){
-    console.log('checkloss')
     if((slots[0].innerText ===slots[1].innerText) && (slots[1].innerText===slots[2].innerText) && (slots[1].innerText==='🦧')){
         const item = slots[1].innerText;
         score += items[item];
